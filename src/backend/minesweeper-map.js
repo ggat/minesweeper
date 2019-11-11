d201ba644c99825a82dc4d033c427002898622db
Giga Gatenashvili <g.gatenashvili@gmail.com>
2019-11-11 18:30:11 +0400

    map with session management and interaction api

--- src/backend/minesweeper-map.js
+++ src/backend/minesweeper-map.js
@@ -1,70 +1,65 @@
-const MINE = -1;
+const
+  SESSION_NONE      = 1,
+  SESSION_STARTED   = 2,
+  SESSION_FAILED    = 3,
+  SESSION_SUCCESS   = 4,
+  BOX_COVERED = -3,
+  BOX_MINE = -2,
+  BOX_EXPLODED = -1,
+  BOX_ZERO = 0
+;
 
 class MineSweeperMap {
-
-  constructor(width, height, minePercentage) {
-
-    if(!Number.isInteger(width)) {
-      throw new Error('MineSweeperMap expects width to be an integer');
-    }
-
-    if(!Number.isInteger(height)) {
-      throw new Error('MineSweeperMap expects height to be an integer');
-    }
-
-    if(Number(minePercentage) !== minePercentage || minePercentage % 1 === 0) {
-      throw new Error('MineSweeperMap expects height to be a float');
-    }
-
-    this.width = width;
-    this.height = height;
-    this.minPercentage = minePercentage;
+  
+  constructor() {
+    this.status = SESSION_NONE;
+    this.mineCount = 0;
+    this.openCount = 0;
   }
 
-  reset() {
-    this.map = [];
+  _reset() {
+    this._map = [];
+    this.boxes = [];
     for (let r = 0; r < this.height; r++) {
-      this.map[r] = [];
-    }
-
-    this.points = [];
-    for (let r = 0; r < this.height; r++) {
+      this._map[r] = [];
       for (let c = 0; c < this.width; c++) {
-        this.points.push([r, c]);
+        this._map[r][c] = BOX_COVERED;
+        this.boxes.push([r, c]);
       }
     }
   }
 
-  setMines() {
-    const mineCount = Math.round(this.minPercentage * this.points.length);
+  _setMines() {
+    this.mineCount = Math.round(this.minPercentage * this.boxes.length);
 
-    for (let i = 0; i < mineCount; i++) {
-      const index = Math.floor(Math.random() * this.points.length);
-      const [r, c] = this.points.splice(index, 1)[0];
-      this.map[r][c] = MINE;
+    for (let i = 0; i < this.mineCount; i++) {
+      const index = Math.floor(Math.random() * this.boxes.length);
+      const [r, c] = this.boxes.splice(index, 1)[0];
+      this._map[r][c] = BOX_MINE;
     }
   }
 
-  setNumbers() {
-    this.points.forEach(([hintR, hintC]) => this.map[hintR][hintC] = this.mineCount(hintR, hintC));
+  _setNumbers() {
+    this.boxes.forEach(([hintR, hintC]) => this._map[hintR][hintC] = this._countMines(hintR, hintC));
   }
 
-  generateMap() {
-    this.reset();
-    this.setMines();
-    this.setNumbers();
-    return this.map;
+  _randomMap() {
+    this._reset();
+    this._setMines();
+    this._setNumbers();
   };
 
-  mineCount(r, c) {
+  _countMines(r, c) {
+    return this._siblingBoxes(r, c).reduce((count, [r, c]) => count + (this._map[r] !== undefined && this._map[r][c] === BOX_MINE), 0);
+  }
+
+  _siblingBoxesIndexes(r, c) {
     const range = [-1, 0, 1];
-    let result = 0;
+    let result = [];
 
     range.forEach(hor => range.forEach(ver => {
         if (!(ver === 0 && hor === 0)) {
-          if (this.map[r - hor] && this.map[r - hor][c - ver] === MINE) {
-            result++;
-          }
+          result.push([r - hor, c - ver]);
         }
       })
     );
@@ -72,9 +67,131 @@ class MineSweeperMap {
     return result;
   }
 
-  toString() {
-    return this.map.map(row => row.map(cell => cell === MINE ? '□' : cell).join());
+  _siblingBoxes(r, c) {
+    return this._siblingBoxesIndexes(r, c).reduce((boxes, [r, c]) => {
+      return this._map[r] !== undefined && this._isValidValue(this._map[r][c]) ? [...boxes, [r, c]] : boxes;
+    }, []);
+  }
+
+  _validateMap(map) {
+    if(!map.length) {
+      throw new Error('MineSweeperMap expects map to have at least one row');
+    }
+
+    if(!map.every(row => row.length === map[0].length)) {
+      throw new Error('MineSweeperMap expects map to same width rows');
+    }
+
+    if(!map.every(row => row.every(cell => this._isValidValue(cell)))) {
+      throw new Error('MineSweeperMap expects map with all boxes having valid value');
+    }
+
+    if(!map.every((row, r) => row.every((cell, c) => {
+      return cell < BOX_ZERO || cell === this._siblingBoxesIndexes(r, c)
+        .reduce((count, [r,c]) => count + (map[r] !== undefined && map[r][c] === BOX_MINE), 0);
+    }))) {
+      throw new Error('MineSweeperMap expects map with all boxes having correct mine count');
+    }
+  }
+
+  _validateArguments(width, height, minePercentage) {
+    if (!Number.isInteger(width)) {
+      throw new Error('MineSweeperMap expects width to be an integer');
+    }
+
+    if (!Number.isInteger(height)) {
+      throw new Error('MineSweeperMap expects height to be an integer');
+    }
+
+    if (Number(minePercentage) !== minePercentage || minePercentage % 1 === 0) {
+      throw new Error('MineSweeperMap expects height to be a float');
+    }
+  }
+
+  _isValidValue(value) {
+    return BOX_COVERED <= value && value <= 8;
+  }
+
+  // public
+  open(x, y) {
+
+    const [r, c] = [y, x];
+    
+    if(this.status !== SESSION_STARTED) {
+      return this.status;
+    }
+
+    if(this._map[r][c] >= BOX_ZERO) {
+      return this.status;
+    }
+    
+    const openingQueue = [[r, c]];
+    while (openingQueue.length) {
+
+      const [r, c] = openingQueue.shift();
+
+      if (this._map[r][c] === BOX_MINE) {
+        this.status = SESSION_FAILED;
+        this._map[r][c] = BOX_EXPLODED;
+        break;
+      }
+
+      this._map[r][c] = this._countMines(r, c);
+      this.openCount++;
+      
+      if( (this.boxes.length - this.mineCount) === this.openCount) {
+        this.status = SESSION_SUCCESS;
+        break;
+      }
+
+      if (this._map[r][c] === BOX_ZERO) {
+        // filter only uncovered and if not already added
+        this._siblingBoxes(r, c).filter(([r, c]) => this._map[r][c] === BOX_COVERED).forEach(([newR, newC]) => {
+          if(!openingQueue.some(([existingR, existingC]) => existingR === newR && existingC === newC)){
+            openingQueue.push([newR, newC]);
+          }
+        });
+      }
+    }
+    
+    return this.status;
+  }
+
+  newSession(width, height, minePercentage) {
+
+    if (Array.isArray(width)) {
+      this._validateMap(width);
+      this._map = width;
+      this.mineCount = this._map.reduce((count, row) => count +
+        row.reduce((count, cell) => count + (cell === BOX_MINE), 0), 0);
+      this.openCount = this._map.reduce((count, row) => count +
+        row.reduce((count, cell) => count + (cell >= BOX_ZERO), 0), 0);
+    } else {
+      this._validateArguments(width, height, minePercentage);
+      this.width = width;
+      this.height = height;
+      this.minPercentage = minePercentage;
+      this._randomMap();
+    }
+
+    this.status = SESSION_STARTED;
+  }
+
+  get currentMap() {
+    return this._map;
   }
 }
 
-module.exports = MineSweeperMap;
+module.exports = {
+  MineSweeperMap,
+  Constants: {
+    SESSION_NONE,
+    SESSION_STARTED,
+    SESSION_FAILED,
+    SESSION_SUCCESS,
+    BOX_COVERED,
+    BOX_MINE,
+    BOX_EXPLODED,
+    BOX_ZERO,
+  },
+};
--- test/backend/minesweeper-map.js
+++ test/backend/minesweeper-map.js
@@ -1,25 +1,59 @@
 const {assert} = require('chai');
-const MineSweeperMap = require('../../src/backend/minesweeper-map');
+const {
+  MineSweeperMap,
+  Constants: {
+    BOX_MINE: M,
+    BOX_COVERED: C
+  }
+} = require('../../src/backend/minesweeper-map');
 
 describe('MineSweeperMap', function () {
 
+  let mapGenerator = new MineSweeperMap();
+
   it('throws error if incorrect parameters passed', function () {
-    assert.throws(() => new MineSweeperMap(), Error);
-    assert.throws(() => new MineSweeperMap(5), Error);
-    assert.throws(() => new MineSweeperMap(5, 5), Error);
-    assert.throws(() => new MineSweeperMap(5.1, 5), Error);
-    assert.throws(() => new MineSweeperMap(5, 5.1), Error);
-    assert.throws(() => new MineSweeperMap(5, 5, 1), Error);
+    assert.throws(() => mapGenerator.newSession(), Error);
+    assert.throws(() => mapGenerator.newSession(5), Error);
+    assert.throws(() => mapGenerator.newSession(5, 5), Error);
+    assert.throws(() => mapGenerator.newSession(5.1, 5), Error);
+    assert.throws(() => mapGenerator.newSession(5, 5.1), Error);
+    assert.throws(() => mapGenerator.newSession(5, 5, 1), Error);
   });
 
-  it('generates two dimensional map', function() {
+  it('throws error if invalid map passed', function () {
+    let map = [];
+    assert.throws(() => mapGenerator.newSession(map), 'expects map to have at least one row');
+
+    map = [
+      [1, 1, 1],
+      [1, -1, 1],
+      [1, 1],
+    ];
+    assert.throws(() => mapGenerator.newSession(map), 'expects map to same width rows');
+
+    map = [
+      [1, 1, C],
+      [1, M, 1],
+      [1, 50, 10],
+    ];
+    assert.throws(() => mapGenerator.newSession(map), 'expects map with all boxes having valid value');
+
+    map = [
+      [1, 1, C],
+      [C, M, 1],
+      [1, 2, 1],
+    ];
+    assert.throws(() => mapGenerator.newSession(map), 'expects map with all boxes having correct mine count');
+  });
+
+  it('generates two dimensional map', function () {
     const
       width = 10,
       height = 10,
       percentage = 0.2;
 
-    let mapGenerator = new MineSweeperMap(width, height, percentage);
-    const map = mapGenerator.generateMap();
+    mapGenerator.newSession(width, height, percentage);
+    const map = mapGenerator.currentMap;
 
     assert(Array.isArray(map));
     assert(map.length === height);
@@ -30,19 +64,51 @@ describe('MineSweeperMap', function () {
     });
   });
 
-  it('spreads correct number of mines', function() {
+  it('spreads correct number of mines', function () {
 
     const
       width = 10,
       height = 10,
       percentage = 0.2;
 
-    let mapGenerator = new MineSweeperMap(width, height, percentage);
-    const map = mapGenerator.generateMap();
+    mapGenerator.newSession(width, height, percentage);
+    const map = mapGenerator.currentMap;
 
     const expectedCount = Math.round(width * height * 0.2);
-    const actualCount = map.reduce((count, row) => count + row.reduce((count, cell) => count + (cell === -1), 0), 0);
+    const actualCount = map.reduce((count, row) =>
+      count + row.reduce((count, cell) => count + (cell === M), 0), 0);
 
     assert(expectedCount === actualCount);
   });
+
+  it('recursively opens zeros', function () {
+    const map = [
+      [C, C, C, C, C],
+      [C, C, C, C, C],
+      [C, M, C, C, C],
+      [C, C, C, C, C],
+      [C, C, C, C, C],
+      [C, C, C, C, C],
+      [C, C, C, M, M],
+    ];
+
+    const expectedOpenedMap = [
+      [0, 0, 0, 0, 0],
+      [1, 1, 1, 0, 0],
+      [C, M, 1, 0, 0],
+      [1, 1, 1, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 2, 2],
+      [0, 0, 1, M, M]
+    ];
+
+    mapGenerator.newSession(map);
+    mapGenerator.open(0, 0);
+
+    for (let r = 0; r < expectedOpenedMap.length; r++) {
+      for (let c = 0; c < expectedOpenedMap[r].length; c++) {
+        assert(expectedOpenedMap[r][c] === mapGenerator.currentMap[r][c]);
+      }
+    }
+  })
 });
